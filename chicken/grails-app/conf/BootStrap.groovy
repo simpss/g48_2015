1fb012cd3fdaa781af44c773d85ca9a5b028a2b0
Siim Talvik <siim.talvik@nortal.com>
2015-02-08 01:08:04 +0200

    added an eatingEnty

--- chicken/grails-app/conf/BootStrap.groovy
+++ chicken/grails-app/conf/BootStrap.groovy
@@ -7,7 +7,7 @@ class BootStrap {
         Chicken chick = new Chicken(birthDate: new Date(1993, 03, 31)).save(flush: true, failOnError: true)
         Chicken chick2 = new Chicken(birthDate: new Date(2003, 02, 22)).save(flush: true, failOnError: true)
 
-        EatingEntry chick1Eat = new EatingEntry(chicken: chick)
+        EatingEntry chick1Eat = new EatingEntry(chicken: chick).save(flush: true, failOnError: true)
     }
     def destroy = {
     }
